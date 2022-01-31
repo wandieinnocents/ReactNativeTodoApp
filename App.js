67207ca3725e450f112132837ea39d7bf5df1857
wandieinnocents <wandieinnocent2@gmail.com>
2022-01-31 11:59:07 -0800

    Add alerts

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import React , { useState }  from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, FlatList } from 'react-native';
+import { StyleSheet, Text, View, FlatList, Alert } from 'react-native';
 import {  YellowBox } from 'react-native-web';
 import Header from './components/Header';
 import TodoItem from './components/TodoItem';
@@ -25,12 +25,24 @@ export default function App() {
   }
 
  const submitHandler = (text) => {
-   setTodos((prevTodos) => {
-     return [
-       { text : text , key: Math.random().toString() },
-       ...prevTodos
-     ];
-   })
+  //  validation 
+  if(text.length > 3){
+    setTodos((prevTodos) => {
+      return [
+        { text : text , key: Math.random().toString() },
+        ...prevTodos
+      ];
+    });
+  }
+  else{
+    Alert.alert('Oops!','Todos must be over 3 chars long',[
+      { text:'Understood', onPress: () => console.log('alert closed') }
+    ]);
+  }
+
+
+
+ 
  }
 
   return (
